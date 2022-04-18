ff79584297fb8621b8c23fec076f9d5dac3b4ebf
Florian Goujeon <florian.goujeon@gmail.com>
2022-04-18 16:30:07 +0200

    run-benchmarks.py: Print results in markdown format

--- run-benchmarks.py
+++ run-benchmarks.py
@@ -9,6 +9,33 @@ import os
 import sys
 import subprocess
 
+class TestResult:
+    def __init__(self, pretty_lib_name, build_time_s, execution_time_s, size_B):
+        self.pretty_lib_name = pretty_lib_name
+        self.build_time_s = build_time_s
+        self.execution_time_s = execution_time_s
+        self.size_B = size_B
+
+def test(pretty_lib_name, lib_name):
+    #Build benchmark
+    start_time = time.time()
+    subprocess.run(["cmake", "--build", build_dir, "--target", "large-" + lib_name])
+    end_time = time.time()
+    build_time_s = end_time - start_time
+
+    executable_path = os.path.join(build_dir, "bin", "large-" + lib_name)
+
+    #Run benchmark
+    start_time = time.time()
+    subprocess.run([executable_path])
+    end_time = time.time()
+    execution_time_s = end_time - start_time
+
+    #Print size
+    size_B = os.path.getsize(executable_path)
+
+    return TestResult(pretty_lib_name, build_time_s, execution_time_s, size_B)
+
 if len(sys.argv) != 2:
     print("Usage: run-benchmarks.py BUILD_DIR")
     exit()
@@ -32,40 +59,17 @@ subprocess.run(cmake_command)
 #Clean
 subprocess.run(["cmake", "--build", build_dir, "--target", "clean"])
 
-class TestResult:
-    def __init__(self, lib_name, build_time_s, execution_time_s, size_B):
-        self.lib_name = lib_name
-        self.build_time_s = build_time_s
-        self.execution_time_s = execution_time_s
-        self.size_B = size_B
+#Run tests
+results = [test("FGFSM", "fgfsm"), test("[Boost::ext].SML", "sml")]
 
-def test(lib_name):
-    #Build benchmark
-    start_time = time.time()
-    subprocess.run(["cmake", "--build", build_dir, "--target", "large-" + lib_name])
-    end_time = time.time()
-    build_time_s = end_time - start_time
-
-    executable_path = os.path.join(build_dir, "bin", "large-" + lib_name)
-
-    #Run benchmark
-    start_time = time.time()
-    subprocess.run([executable_path])
-    end_time = time.time()
-    execution_time_s = end_time - start_time
-
-    #Print size
-    size_B = os.path.getsize(executable_path)
-
-    return TestResult(lib_name, build_time_s, execution_time_s, size_B)
-
-results = [test("fgfsm"), test("sml")]
-
+#Print test results in markdown format
+print("| | Build time | Execution time | Binary size")
+print("|--|--|--|--")
 for res in results:
     print(
-        '%s: %0.3f s, %0.3f s, %d B' %
+        '| **%s** | %0.3f s | %0.3f s | %d B' %
         (
-            res.lib_name,
+            res.pretty_lib_name,
             res.build_time_s,
             res.execution_time_s,
             res.size_B
